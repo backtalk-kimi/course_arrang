15f3e4e3dea9f662dfdeebdfad529a80293333e9
backtalk-kimi <892492784@qq.com>
2021-09-18 12:18:10 +0800

    输入优化

--- data_entire.py
+++ data_entire.py
@@ -49,16 +49,77 @@ class education_plan():
                 course_room = random.randint(1,self.class_num)
                 temp = course_range(self.teachers[id],self.courses[id],course_time,course_room)
                 l.append(temp)
+        self.arrange = l
+        self.entity_count = len(self.arrange)
         return l
 
-def list_display(l):
-    length = len(l)
-    for id in range(length):
-        print("teacher:%d\tcourse:%d\ttime:%d\troom:%d"%(l[id].teacher,l[id].course,l[id].time,l[id].room))
-    return
+    # 变异过程程序
+    def variation(self, rate):
+        l = self.arrange
+        num = self.entity_count
 
+        # print("count = %d"%(num))
 
+        var_num = int(rate * num)
+        var_list = random.sample(range(num),var_num)
+        for id in range(var_num):
+            var = var_list[id]
+            print("varnum:%d\tteacher:%d\tcourse:%d\ttime:%d\troom:%d" % (var,l[var].teacher, l[var].course, l[var].time, l[var].room))
+            l[var].time = random.randint(1, self.time)
+            l[var].room = random.randint(1, self.time)
+            print("teacher:%d\tcourse:%d\ttime:%d\troom:%d" % (l[var].teacher, l[var].course, l[var].time, l[var].room))
+        self.arrange = l
+        return l
+
+    def list_display(self):
+        l = self.arrange
+        length = self.entity_count
+        for id in range(length):
+            print("teacher:%d\tcourse:%d\ttime:%d\troom:%d" % (l[id].teacher, l[id].course, l[id].time, l[id].room))
+        return
+
+# class arrange():
+#     def __init__(self):
+#         l = list()
+#         for id in range(self.length):
+#             for course_id in range(self.course_num_week[id]):
+#                 course_time = random.randint(1,self.time)
+#                 course_room = random.randint(1,self.class_num)
+#                 temp = course_range(self.teachers[id],self.courses[id],course_time,course_room)
+#                 l.append(temp)
+#         self.arrange = l
+#         self.entity_count = len(self.arrange)
+#         return l
+#
+#     # 变异过程程序
+#     def variation(self, rate):
+#         l = self.arrange
+#         num = self.entity_count
+#
+#         # print("count = %d"%(num))
+#
+#         var_num = int(rate * num)
+#         var_list = random.sample(range(num),var_num)
+#         for id in range(var_num):
+#             var = var_list[id]
+#             print("varnum:%d\tteacher:%d\tcourse:%d\ttime:%d\troom:%d" % (var,l[var].teacher, l[var].course, l[var].time, l[var].room))
+#             l[var].time = random.randint(1, self.time)
+#             l[var].room = random.randint(1, self.time)
+#             print("teacher:%d\tcourse:%d\ttime:%d\troom:%d" % (l[var].teacher, l[var].course, l[var].time, l[var].room))
+#         self.arrange = l
+#         return l
+#
+#     def list_display(self):
+#         l = self.arrange
+#         length = self.entity_count
+#         for id in range(length):
+#             print("teacher:%d\tcourse:%d\ttime:%d\troom:%d" % (l[id].teacher, l[id].course, l[id].time, l[id].room))
+#         return
 
 plan = education_plan()
 plan_list = education_plan.list_generation(plan)
-list_display(plan_list)
+print("教学安排初始情况：")
+education_plan.list_display(plan)
+plan_list = education_plan.variation(plan,0.2)
+print("变异之后情况：")
+education_plan.list_display(plan)
